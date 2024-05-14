e787b04284fcbdb1d74ec2576668e88d23e7bc1f
unknown <jongarcia.realty@gmail.com>
2024-05-14 14:30:06 -0700

    added controllers/schemas

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+})
